a9900d99cfe5b0f1d2f3b4212346b204bb787a59
zawnaing11 <znttestpro@gmail.com>
2019-06-26 20:23:37 +0630

    #19 permission

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
+
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -19,12 +20,24 @@ class UsersTableSeeder extends Seeder
 
         DB::table('roles')->insert([
             'name' => 'admin',
-            'guard_name' => 'web'
+            'guard_name' => 'web',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
         ]);
 
         DB::table('roles')->insert([
             'name' => 'master',
-            'guard_name' => 'web'
+            'guard_name' => 'web',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ]);
+
+        DB::table('model_has_roles')->insert([
+            'role_id' => 1,
+            'model_id' => 1,
+            'model_type' => 'App\User',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
         ]);
     }
 }
